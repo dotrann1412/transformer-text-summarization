f111c904427676113c7877dadc27393098d1bd82
huynhtuan17ti <huynhminhtuan6429@gmail.com>
2023-06-03 22:40:09 +0700

    add initial webpage code

---
+++ web/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-config-provider namespace="ep">
+    <Logos />
+    <div class="flex main-container">
+      <Main />
+    </div>
+  </el-config-provider>
+</template>
+
+<style>
+#app {
+  text-align: center;
+  color: var(--ep-text-color-primary);
+}
+
+.main-container {
+  height: calc(100vh - var(--ep-menu-item-height) - 3px);
+}
+</style>
---
+++ web/src/components/Main.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <el-input
+      class="text-container"
+      v-model="textarea1"
+      type="textarea"
+      rows="40"
+      placeholder="Please input"
+    />
+
+    <el-button type="primary" round :disabled="textarea1.length == 0">Summarize</el-button>
+
+    <el-input
+      class="text-container"
+      v-model="textarea2"
+      type="textarea"
+      rows="40"
+      placeholder="Summarized text"
+      disabled
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const textarea1 = ref('')
+const textarea2 = ref('')
+</script>
+
+<style scoped lang="scss">
+.container {
+  margin: auto;
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  .text-container {
+    padding: 30px;
+    width: 45vw;
+  }
+}
+</style>
