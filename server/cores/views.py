bbebd9a93688e9daa5c4b6e3b46ecd0f9302fa5f
dotrann1412 <dotrann.1412@gmail.com>
2023-06-05 23:57:04 +0700

    change settings

--- server/cores/views.py
+++ server/cores/views.py
@@ -4,21 +4,52 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from django.http import JsonResponse
 from rest_framework import status
+from enum import Enum
 
-from text_summarization.basic import summarize as basic_summarize
+from text_summarization.summarizer import use_word_frequency as basic_summarize
 
 class TextSummarizer(APIView):
+    class Summarizer(str, Enum):
+        BASIC = "basic"
+        BERT = "bert"
+        T5 = "t5"
+        GPT = "gpt"
+        
+    def get(self, request):
+        return JsonResponse({
+            "message": "Text Summarization API",
+            "notes": [
+                "This endpoint is used to summarize the given document or text.",
+                "There are three summarization methods available:",
+                "    1. Basic Summarization. Using frequency table and statistical method",
+                "    2. BERT Summarization base",
+                "    3. T5 Summarization base",
+                "    4. GPT Summarization base, GPT 2 is used for now",
+                "To use the service, send a POST request to this endpoint with the following data:",
+                "    1. text: the text to be summarized",
+                "    2. keep: the percentage of the text to be kept (default: 0.3)",
+                "    3. summarizer (basic | bert | t5 | gpt 2): the summarization method to be used (default: t5)",
+            ]
+        }, indent = 4, status=status.HTTP_200_OK)
+
     def post(self, request):
         try:
             data = request.data
-            text = data["text"]
+            text, method, keep = data.get("text", None), TextSummarizer.Summarizer(data.get("method", "t5")), data.get('keep', 0.3)
+            
+            if type(keep) != float:
+                try: keep = float(keep)
+                except: keep = 0.3
 
+            if not text:
+                raise "Error while processing the requests, text should be provided. If something is going wrong, remind us via [link](https://google.com)."
+            
             return JsonResponse({
                 "summary": basic_summarize(
                     text, 
-                    data.get('keep', 0.3)
+                    keep
                 )
             }, status=status.HTTP_200_OK)
 
         except Exception as e:
-            return JsonResponse({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return JsonResponse({"summary": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- server/main/settings.py
+++ server/main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-w%rqey(wfa%rbi0_801^9(fdb5qm##n(@%cubato)5om0!1au8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
