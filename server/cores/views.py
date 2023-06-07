e24f80fa03fd866475884fd2821e7bf9a3f5b6eb
do.tranngoc <dotrann.1412@gmail.com>
2023-06-08 00:13:51 +0700

    update file

--- server/cores/views.py
+++ server/cores/views.py
@@ -18,19 +18,18 @@ class TextSummarizer(APIView):
     def get(self, request):
         return JsonResponse({
             "message": "Text Summarization API",
-            "notes": [
-                "This endpoint is used to summarize the given document or text.",
-                "There are three summarization methods available:",
-                "    1. Basic Summarization. Using frequency table and statistical method",
-                "    2. BERT Summarization base",
-                "    3. T5 Summarization base",
-                "    4. GPT Summarization base, GPT 2 is used for now",
-                "To use the service, send a POST request to this endpoint with the following data:",
-                "    1. text: the text to be summarized",
-                "    2. keep: the percentage of the text to be kept (default: 0.3)",
-                "    3. summarizer (basic | bert | t5 | gpt 2): the summarization method to be used (default: t5)",
-            ]
-        }, indent = 4, status=status.HTTP_200_OK)
+            "notes":
+                "This endpoint is used to summarize the given document or text.\n"
+                "There are three summarization methods available:\n"
+                "    1. Basic Summarization. Using frequency table and statistical method\n"
+                "    2. BERT Summarization base\n"
+                "    3. T5 Summarization base\n"
+                "    4. GPT Summarization base, GPT 2 is used for now\n"
+                "To use the service, send a POST request to this endpoint with the following data:\n"
+                "    1. text: the text to be summarized\n"
+                "    2. keep: the percentage of the text to be kept (default: 0.3)\n"
+                "    3. summarizer (basic | bert | t5 | gpt 2): the summarization method to be used (default: t5)\n"
+        }, status=status.HTTP_200_OK)
 
     def post(self, request):
         try:
